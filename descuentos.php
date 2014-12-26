d8e3bf604863e8f7b3d8957e48da8325f634fd0d
Franco Ushiñahua Reátegui <fraure13@gmail.com>
2014-12-26 18:51:55 -0500

    only head

--- descuentos.php
+++ descuentos.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once '/app/start.php'; //Start para facebook -> ;)
-require_once '/app/funciones.php';
+require_once 'head.php'; //Start para facebook -> ;)
+
 $db = Spdo::singleton();
 
 //$host="http://".$_SERVER['SERVER_NAME']."/md";
@@ -103,71 +103,7 @@ $stmt = $db->prepare("SELECT p.idpublicaciones,p.titulo1, p.titulo2, p.descripci
 }        
 
 ?>
-<!DOCTYPE html>
-<html class="noIE" lang="es">
-<head>
-<meta charset="UTF-8">
-<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="Viewport">
-<meta content="Muchos Descuentos" name="description">
-<link rel="shortcut icon" type="image/x-icon" href="<?php echo $host; ?>/favicon.ico">
-<!--<meta content="logoby.us" name="author">-->
-<title><?php echo $st;?></title>
 
-<!-- Reset CSS -->
-<link href="<?php echo $host; ?>/css/normalize.css" rel="stylesheet" type="text/css"/>
-
-<!-- Bootstrap core CSS -->
-<link href="<?php echo $host; ?>/css/bootstrap.css" rel="stylesheet">
-
-<!-- IView Slider CSS -->
-<link href="<?php echo $host; ?>/css/iView.css" rel="stylesheet">
-
-
-<link href="<?php echo $host; ?>/css/micss.css" rel="stylesheet"/>
-<link type="text/css" href="<?php echo $host; ?>/css/jquery-ui.min.css" rel="stylesheet" />
-<link type="text/css" href="<?php echo $host; ?>/css/jquery-ui.structure.min.css" rel="stylesheet" />
-<link type="text/css" href="<?php echo $host; ?>/css/jquery-ui.theme.min.css" rel="stylesheet" />
-
-<!-- Animations -->
-<link href="<?php echo $host; ?>/css/animate.css" rel="stylesheet" type="text/css"/>
-
-<!-- Custom styles for this template -->
-<link href="<?php echo $host; ?>/css/custom.css" rel="stylesheet" type="text/css" />
-
-<!-- Style Switcher -->
-<link href="<?php echo $host; ?>/css/style-switch.css" rel="stylesheet" type="text/css"/>
-
-<!-- Color -->
-<link href="<?php echo $host; ?>/css/skin/color.css" id="colorstyle" rel="stylesheet">
-
-<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]> <script src="js/html5shiv.js"></script> <script src="js/respond.min.js"></script> <![endif]-->
-
-<!-- Bootstrap core JavaScript -->
-<script type="text/javascript" src="<?php echo $host; ?>/js/jquery-1.11.1.min.js"></script>
-<script type="text/javascript" src="<?php echo $host; ?>/js/jquery-ui-1.10.3.custom.min.js"></script>    
-<script src="<?php echo $host; ?>/js/bootstrap.min.js"></script>
-<script src="<?php echo $host; ?>/js/bootstrap-select.js"></script>
-
-<!-- Custom Scripts -->
-<script src="<?php echo $host; ?>/js/scripts.js"></script>
-
-<!-- iView Slider -->
-<script src="<?php echo $host; ?>/js/raphael-min.js" type="text/javascript"></script>
-<script src="<?php echo $host; ?>/js/jquery.easing.js" type="text/javascript"></script>
-<script src="<?php echo $host; ?>/js/iView.js" type="text/javascript"></script>
-<script src="<?php echo $host; ?>/js/retina-1.1.0.min.js" type="text/javascript"></script>
-<script>
-  !window.jQuery && document.write("<script src='<?php echo $host; ?>/js/jquery.min.js'><\/script>")
-</script>
-<!--[if IE 8]>
-    <script type="text/javascript" src="js/selectivizr.js"></script>
-    <![endif]-->
-
-<script src="<?php echo $host;?>/js/mijs/js.js" type="text/javascript"></script>
-<script src="<?php echo $host; ?>/js/mijs/publicaciones.js" type="text/javascript"></script>
-
-</head>
 <body>
 <div id="frm-suscripcion"></div>
 <header>   
---
+++ head.php
@@ -0,0 +1,49 @@
+<?php 
+require_once '/app/start.php'; //Start para facebook -> ;)
+require_once '/app/funciones.php';
+?>
+<!DOCTYPE html>
+<html class="noIE" lang="es">
+<head>
+<meta charset="UTF-8">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="Viewport">
+<meta content="Muchos Descuentos" name="description">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<title>Muchos Descuentos</title>
+
+<link href="<?php echo $host; ?>/css/bootstrap.css" rel="stylesheet">
+<link href="<?php echo $host; ?>/css/iView.css" rel="stylesheet">
+<link href="<?php echo $host; ?>/css/micss.css" rel="stylesheet"/>
+<link type="text/css" href="<?php echo $host; ?>/css/jquery-ui.min.css" rel="stylesheet" />
+<link type="text/css" href="<?php echo $host; ?>/css/jquery-ui.structure.min.css" rel="stylesheet" />
+<link type="text/css" href="<?php echo $host; ?>/css/jquery-ui.theme.min.css" rel="stylesheet" />
+<!-- Animations -->
+<!-- Custom styles for this template -->
+<link href="<?php echo $host; ?>/css/custom.css" rel="stylesheet" type="text/css" />
+<!-- Style Switcher -->
+<link href="<?php echo $host; ?>/css/style-switch.css" rel="stylesheet" type="text/css"/>
+<!-- Color -->
+<link href="<?php echo $host; ?>/css/skin/color.css" id="colorstyle" rel="stylesheet">
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]> <script src="js/html5shiv.js"></script> <script src="js/respond.min.js"></script> <![endif]-->
+<!-- Bootstrap core JavaScript -->
+
+<script type="text/javascript" src="<?php echo $host; ?>/js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="<?php echo $host; ?>/js/jquery-ui-1.10.3.custom.min.js"></script>    
+<script src="<?php echo $host; ?>/js/bootstrap.min.js"></script>
+
+<!-- iView Slider -->
+<script src="<?php echo $host; ?>/js/raphael-min.js" type="text/javascript"></script>
+<script src="<?php echo $host; ?>/js/jquery.easing.js" type="text/javascript"></script>
+<script src="<?php echo $host; ?>/js/iView.js" type="text/javascript"></script>
+<script src="<?php echo $host; ?>/js/retina-1.1.0.min.js" type="text/javascript"></script>
+<script>
+  !window.jQuery && document.write("<script src='js/jquery.min.js'><\/script>")
+</script>
+<!--[if IE 8]>
+    <script type="text/javascript" src="js/selectivizr.js"></script>
+<![endif]-->
+<script type="text/javascript" src="<?php echo $host; ?>/js/utilitarios.js"></script>
+<script type="text/javascript" src="<?php echo $host; ?>/js/mijs/js.js"></script>
+<script type="text/javascript" src="<?php echo $host; ?>/js/mijs/publicaciones.js"></script>
+</head>
